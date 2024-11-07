0da2f5964550cba70e98af99b6ec3e8e9bb80889
AndreFerreira18 <andrefferreira@outlook.pt>
2024-11-07 17:54:46 +0000

    Code cleaning on MainActivity

--- app/src/main/java/com/afferreira/dev/todoapp/MainActivity.kt
+++ app/src/main/java/com/afferreira/dev/todoapp/MainActivity.kt
@@ -23,7 +23,5 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
-    }
+    override fun onSupportNavigateUp(): Boolean = navController.navigateUp() || super.onSupportNavigateUp()
 }
