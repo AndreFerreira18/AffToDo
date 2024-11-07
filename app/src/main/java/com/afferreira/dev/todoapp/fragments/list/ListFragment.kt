57d11afb1f92564c762e983b0c938aba763e0cc3
AndreFerreira18 <andrefferreira@outlook.pt>
2024-11-07 16:35:05 +0000

    feat: Completed Sessions 5, 6, 7, 8, 9 and 10.

--- app/src/main/java/com/afferreira/dev/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/afferreira/dev/todoapp/fragments/list/ListFragment.kt
@@ -9,20 +9,24 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SearchView
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.afferreira.dev.todoapp.R
+import com.afferreira.dev.todoapp.data.models.ToDoData
 import com.afferreira.dev.todoapp.data.viewmodel.ToDoViewModel
 import com.afferreira.dev.todoapp.databinding.FragmentListBinding
 import com.afferreira.dev.todoapp.fragments.SharedViewModel
 import com.afferreira.dev.todoapp.fragments.list.adapter.ListAdapter
-import com.afferreira.dev.todoapp.utils.observeOnce
+import com.google.android.material.snackbar.Snackbar
+import jp.wasabeef.recyclerview.animators.LandingAnimator
 
 
 class ListFragment : Fragment(), SearchView.OnQueryTextListener {
@@ -60,6 +64,13 @@ class ListFragment : Fragment(), SearchView.OnQueryTextListener {
         menuHost.addMenuProvider(object : MenuProvider {
             override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
                 menuInflater.inflate(R.menu.list_fragment_menu, menu)
+
+                // Search Listener
+                val search = menu.findItem(R.id.menu_search)
+                val searchView = search.actionView as? SearchView
+                // Enable Submit on search bar
+                // searchView?.isSubmitButtonEnabled = true
+                searchView?.setOnQueryTextListener(this@ListFragment)
             }
 
             override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
@@ -84,13 +95,16 @@ class ListFragment : Fragment(), SearchView.OnQueryTextListener {
     private fun setupRecyclerview() {
         val recyclerView = binding.recyclerView
         recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(requireContext()) // StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        recyclerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL) //LinearLayoutManager(requireContext())
+        recyclerView.itemAnimator = LandingAnimator().apply {
+            addDuration = 300
+        }
 
         // Swipe to Delete
-//        swipeToDelete(recyclerView)
+        swipeToDelete(recyclerView)
     }
 
-    /* private fun swipeToDelete(recyclerView: RecyclerView) {
+    private fun swipeToDelete(recyclerView: RecyclerView) {
          val swipeToDeleteCallback = object : SwipeToDelete() {
              override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                  val deletedItem = adapter.dataList[viewHolder.adapterPosition]
@@ -98,23 +112,24 @@ class ListFragment : Fragment(), SearchView.OnQueryTextListener {
                  mToDoViewModel.deleteItem(deletedItem)
                  adapter.notifyItemRemoved(viewHolder.adapterPosition)
                  // Restore Deleted Item
-                 restoreDeletedData(viewHolder.itemView, deletedItem)
+                 restoreDeletedData(viewHolder.itemView, deletedItem, viewHolder.adapterPosition)
              }
          }
          val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallback)
          itemTouchHelper.attachToRecyclerView(recyclerView)
-     }*/
+     }
 
-    /*private fun restoreDeletedData(view: View, deletedItem: ToDoData) {
+    private fun restoreDeletedData(view: View, deletedItem: ToDoData, position: Int) {
         val snackBar = Snackbar.make(
             view, "Deleted '${deletedItem.title}'",
             Snackbar.LENGTH_LONG
         )
         snackBar.setAction("Undo") {
             mToDoViewModel.insertData(deletedItem)
+            adapter.notifyItemChanged(position)
         }
         snackBar.show()
-    }*/
+    }
 
     override fun onQueryTextSubmit(query: String?): Boolean {
         if (query != null) {
@@ -133,8 +148,8 @@ class ListFragment : Fragment(), SearchView.OnQueryTextListener {
     private fun searchThroughDatabase(query: String) {
         val searchQuery = "%$query%"
 
-        mToDoViewModel.searchDatabase(searchQuery).observeOnce(viewLifecycleOwner) { list ->
-            list.let {
+        mToDoViewModel.searchDatabase(searchQuery).observe(viewLifecycleOwner) { list ->
+            list?.let {
                 Log.d("ListFragment", "searchThroughDatabase")
                 adapter.setData(it)
             }
--- app/src/main/java/com/afferreira/dev/todoapp/fragments/list/adapter/ToDoDiffUtil.kt
+++ app/src/main/java/com/afferreira/dev/todoapp/fragments/list/adapter/ToDoDiffUtil.kt
@@ -8,22 +8,16 @@ class ToDoDiffUtil(
     private val newList: List<ToDoData>
 ): DiffUtil.Callback() {
 
-    override fun getOldListSize(): Int {
-        return oldList.size
-    }
+    override fun getOldListSize(): Int = oldList.size
 
-    override fun getNewListSize(): Int {
-        return newList.size
-    }
 
-    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return oldList[oldItemPosition] === newList[newItemPosition]
-    }
+    override fun getNewListSize(): Int = newList.size
 
-    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return oldList[oldItemPosition].id == newList[newItemPosition].id
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean = oldList[oldItemPosition] === newList[newItemPosition]
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean = oldList[oldItemPosition].id == newList[newItemPosition].id
                 && oldList[oldItemPosition].title == newList[newItemPosition].title
                 && oldList[oldItemPosition].description == newList[newItemPosition].description
                 && oldList[oldItemPosition].priority == newList[newItemPosition].priority
-    }
 }
